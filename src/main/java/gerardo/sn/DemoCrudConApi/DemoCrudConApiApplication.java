644ee18bf9c96effd33f22ca89dba16e889d2275
gerardosn <gerardo.sena@gmail.com>
2024-11-28 20:07:17 -0300

    Initial commit

---
+++ src/main/java/gerardo/sn/DemoCrudConApi/DemoCrudConApiApplication.java
@@ -0,0 +1,13 @@
+package gerardo.sn.DemoCrudConApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudConApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudConApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gerardo/sn/DemoCrudConApi/DemoCrudConApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gerardo.sn.DemoCrudConApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudConApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
